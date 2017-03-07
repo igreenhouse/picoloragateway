4b04355f523d53babe47f77697ac8fb012ae9759
Stractus <charles-marchand@hotmail.fr>
2017-03-07 16:22:21 +0100

    Version compatible avec DHT11

--- Pycom.py
+++ Pycom.py
@@ -5,14 +5,17 @@ lora=LoRa(mode=LoRa.LORA,frequency=868000000, sf=7)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW) #create a LoRa socket
 i=0
 while 1:
-    t=s.recv(4)
-    rx_data= struct.unpack('f', t)
-    print(rx_data)
-    if i==1:
+    if i==0:
+        t=s.recv(4)
+        rx_data= struct.unpack('f', t)
         temperature = rx_data
-    elif i==2:
+    elif i==1:
+        t=s.recv(4)
+        rx_data= struct.unpack('f', t)
         humidite = rx_data
-    elif i==3:
+    elif i==2:
+        t=s.recv(4)
+        rx_data= struct.unpack('f', t)
         numero=rx_data
         print("Releve, temperature : ")
         print(temperature)
@@ -20,4 +23,6 @@ while 1:
         print(humidite)
         print("numero : ")
         print(numero)
+    elif i==3:
+        t=s.recv(4)
     i = (i+1)%4
